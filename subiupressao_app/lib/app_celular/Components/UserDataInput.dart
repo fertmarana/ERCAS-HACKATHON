b6c3769f178fccff5435b904d35016f0ebdf6ae4
lucascardoso910 <lucascardoso910@gmail.com>
2022-02-03 08:59:58 -0300

    Add Login screen
    Searches if the user JSON file exists, if so, jumps to App widget. If it
    doesn't exist, asks User for name and age and creates this file and
    instance of the User class.
    
    Signed-off-by: lucascardoso910 <lucascardoso910@gmail.com>

---
+++ subiupressao_app/lib/app_celular/Components/UserDataInput.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UserDataInput extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType keyboard;
+  final List<TextInputFormatter> filter;
+  final String fieldName;
+  final String hintString;
+  final double spacerInterval;
+  final bool enabled;
+
+  const UserDataInput({
+    Key key,
+    this.controller,
+    this.keyboard = TextInputType.text,
+    this.filter = const [],
+    this.fieldName,
+    this.hintString,
+    this.enabled = true,
+    this.spacerInterval = 0.05,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Column(children: [
+      Row(
+        children: [
+          SizedBox(width: spacerInterval * size.width),
+          Text(fieldName, style: TextStyle(fontSize: 16)),
+          Spacer()
+        ],
+      ),
+      TextField(
+        controller: this.controller,
+        enabled: this.enabled,
+        keyboardType: this.keyboard,
+        inputFormatters: this.filter,
+        decoration: InputDecoration(
+          constraints: BoxConstraints(maxWidth: size.width * 0.9),
+          border: OutlineInputBorder(),
+          hintText: this.hintString,
+          suffixIcon: IconButton(
+            onPressed: () {
+              this.controller.clear();
+            },
+            icon: const Icon(Icons.clear),
+          ),
+        ),
+      ),
+    ]);
+  }
+}
---
+++ subiupressao_app/lib/app_celular/Login/Login.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io' as io;
+
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/src/provider.dart';
+import 'package:subiupressao_app/app.dart';
+import 'package:subiupressao_app/app_celular/Components/ProfilePicture.dart';
+import 'package:subiupressao_app/app_celular/Components/UserDataInput.dart';
+import 'package:subiupressao_app/files/FileController.dart';
+import 'package:subiupressao_app/files/models/medicine.dart';
+import 'package:subiupressao_app/files/models/user.dart';
+
+class LoginPage extends StatelessWidget {
+  final _ageController = TextEditingController();
+  final _nameController = TextEditingController();
+  User _user;
+
+  @override
+  Widget build(BuildContext context) {
+    _user = context.select((FileController controller) {
+      controller.readUser();
+      return controller.user;
+    });
+
+    if (_user == null) {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            SizedBox(height: 80),
+            Image.asset('imagens/SubiuPressao.png'),
+            SizedBox(height: 30),
+            SizedBox(
+              height: 520,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 30),
+                    Text("Cadastre-se", style: TextStyle(fontSize: 24)),
+                    UserDataInput(
+                      controller: _nameController,
+                      fieldName: "Nome",
+                      hintString: "Insira seu nome aqui",
+                    ),
+                    UserDataInput(
+                      controller: _ageController,
+                      keyboard: TextInputType.number,
+                      filter: [FilteringTextInputFormatter.digitsOnly],
+                      fieldName: "Idade",
+                      hintString: "Insira sua idade aqui",
+                    ),
+                    SizedBox(height: 15),
+                    ElevatedButton(
+                      child: Text(
+                        "Confirmar",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      onPressed: () {
+                        FileController controller = new FileController();
+                        controller.writeUser(
+                          _nameController.text,
+                          int.parse(_ageController.text),
+                          [
+                            Medicine(
+                                name: "Exemplo",
+                                quantity: 0,
+                                start: DateTime.now(),
+                                end: DateTime.now())
+                          ],
+                        );
+
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => App()),
+                            (route) => false);
+                      },
+                    ),
+                    SizedBox(height: 15),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return App();
+    }
+  }
+}
